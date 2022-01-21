a14b27f7df02ba715eb1066f0599d5f58f62ec27
Pdantasjr <paulodantas19@gmail.com>
2022-01-21 16:59:43 -0300

    inicio da alteração do layout do dashboard

---
+++ resources/js/Layouts/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div class="mt-8 text-2xl">
+                <h1>Sidebar</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,15 +1,25 @@
 <template>
     <app-layout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+        <aside class="bg-gray-600 col-span-2 h-full">
+            <div class="">
+                <ul>
+                    <li>Teste</li>
+                    <li>teste</li>
+                    <li>teste</li>
+                </ul>
+            </div>
+        </aside>
+        <main class="bg-gray-500 col-span-10">
+            Conteúdos principais
+        </main>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <welcome />
+                </div>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <sidebar />
                 </div>
             </div>
         </div>
@@ -20,11 +30,13 @@
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
     import Welcome from '@/Jetstream/Welcome.vue'
+    import Sidebar from '@/Layouts/Sidebar.vue'
 
     export default defineComponent({
         components: {
             AppLayout,
             Welcome,
+            Sidebar,
         },
     })
 </script>
